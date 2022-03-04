0f60eacfdf5b0bd4d701b792f2fdf8b7b886c728
MorenaNobre <morenagnobre@gmail.com>
2022-03-04 13:57:20 -0300

    Components Header / Pagination / Sidebar

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,20 @@
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+
+export function Profile() {
+  return (
+    <Flex align="center">
+      <Box mr="4" textAlign="right">
+        <Text>Morena Nobre</Text>
+        <Text color="gray.300" fontSize="small">
+          morenagnobre@gmail.com
+        </Text>
+      </Box>
+
+      <Avatar
+        size="md"
+        name="Morena Nobre"
+        src="https://github.com/morenanobre.png"
+      />
+    </Flex>
+  );
+}
